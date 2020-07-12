e29428adc645666ce388445cbfba92cf79220a71
vdnj <dinatalevalentin@gmail.com>
2020-07-12 21:36:38 +0200

    Updates

---
+++ precache-manifest.ea28d4c098168a1da0fe50ac379f2a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e05b6ab9568fa75447c814db65b40c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8a37b57164c83fd82b59",
+    "url": "/monsters-rolodex/static/css/main.af9a50a2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a37b57164c83fd82b59",
+    "url": "/monsters-rolodex/static/js/main.2124efcb.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
